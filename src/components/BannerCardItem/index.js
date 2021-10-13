4a20e67193d64aa46211df3f61bf0bd1340a4d04
Dheeraj Meena <codingwithdheeraj@gmail.com>
2021-10-13 19:09:29 +0000

    First commit

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const BannerElement = props => {
+  const {bannerCard} = props
+  // eslint-disable-next-line
+  const {headerText, description, className, id} = bannerCard
+
+  return (
+    <li className={className}>
+      <h1 className="banner-heading"> {headerText} </h1>
+      <p className="banner-description"> {description} </p>
+      <button className="show-more-button"> Show More</button>
+    </li>
+  )
+}
+
+export default BannerElement
